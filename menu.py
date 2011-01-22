1432dc30da63f746db40a24dcbc5ebd7a5bb5ba3
Issac Kelly <issac.kelly@gmail.com>
2011-01-22 10:56:34 -0500

    Menu Fix

---
+++ menu.py
@@ -0,0 +1,44 @@
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+from admin_tools.menu import items, Menu
+from admin_tools.menu.models import BookmarkMenuItem
+
+# to activate your custom menu add the following to your settings.py:
+#
+# ADMIN_TOOLS_MENU = "weserve.menu.CustomMenu"
+
+class CustomMenu(Menu):
+    """
+    Custom Menu for weserve admin site.
+    """
+    def __init__(self, **kwargs):
+        Menu.__init__(self, **kwargs)
+        self.children.append(items.MenuItem(
+            title=_("Dashboard"),
+            url=reverse("admin:index")
+        ))
+        self.children.append(items.MenuItem(
+            title=_("Back To Site"),
+            url=reverse("reg_home")
+        ))
+        self.children.append(items.MenuItem(
+            title=_("Inbox"),
+            url=reverse("msgs_inbox")
+        ))
+        self.children.append(BookmarkMenuItem(
+            title=_("Bookmarks")
+        ))
+        self.children.append(items.AppList(
+            title=_("Administration"),
+            exclude_list=("django.contrib",)
+        ))
+        self.children.append(items.AppList(
+            title=_("Administration"),
+            include_list=("django.contrib",)
+        ))
+
+    def init_with_context(self, context):
+        """
+        Use this method if you need to access the request context.
+        """
+        pass
