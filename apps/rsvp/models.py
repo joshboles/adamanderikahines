cddd0dcf120d08a8a137fd2c9c394bdcd1832d5b
Josh Boles <jaboles@gmail.com>
2011-01-28 09:55:30 -0500

    Updated copy.

--- apps/rsvp/models.py
+++ apps/rsvp/models.py
@@ -5,6 +5,6 @@ class Rsvp(TimeStampedModel):
     dinner_dancing = models.BooleanField("Will you be joining us for Dinner & Dancing on June 3rd?")
     how_many = models.IntegerField("How many will be attending?", blank=True, null=True)
     names = models.CharField("Name or names of who will be attending", max_length=1024, blank=True, null=True)
-    stuffed_turkey_roulade = models.IntegerField(default=0, help_text="Sliced turkey breast stuffed with pine nuts, carrots, spinach & red onions. Served with an orange and terragon burgundy sauce.")
+    stuffed_turkey_roulade = models.IntegerField(default=0, help_text="Sliced turkey breast stuffed with pine nuts, carrots, spinach & red onions. Served with an orange and terragon burgundy sauce. (The Bride and Grooms favorite)")
     citrus_grilled_salmon = models.IntegerField(default=0, help_text="Grilled salmon fillet with a tri-colored peppercorn citrus sauce.")
     comments = models.TextField("Any Questions or Comments", blank=True, null=True)
