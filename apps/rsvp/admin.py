0d16018142ebb1448b0042fd63b4e3bbf4c2f020
Josh Boles <jaboles@gmail.com>
2011-02-10 16:52:14 -0500

    Bleh

--- apps/rsvp/admin.py
+++ apps/rsvp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from rsvp.models import Rsvp
 
-class RsvpAdmin:
+class RsvpAdmin(admin.ModelAdmin):
     list_display = ["dinner_dancing", "how_many", "names", "stuffed_turkey_roulade", "citrus_grilled_salmon"]
 
 admin.site.register(Rsvp, RsvpAdmin)
