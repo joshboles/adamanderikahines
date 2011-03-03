92f2105c15d31f32a38d8486541d4b6779f4c4eb
Issac Kelly <issac.kelly@gmail.com>
2011-03-03 16:25:02 -0500

    model changes

--- apps/rsvp/models.py
+++ apps/rsvp/models.py
@@ -3,8 +3,14 @@ from model_utils.models import TimeStampedModel
 
 class Rsvp(TimeStampedModel):
     dinner_dancing = models.BooleanField("Will you be joining us for Dinner & Dancing on June 3rd?")
-    how_many = models.IntegerField("How many will be attending?", blank=True, null=True)
-    names = models.CharField("Name or names of who will be attending", max_length=1024, blank=True, null=True)
-    stuffed_turkey_roulade = models.IntegerField(default=0, help_text="Sliced turkey breast stuffed with pine nuts, carrots, spinach & red onions. Served with an orange and terragon burgundy sauce.")
-    citrus_grilled_salmon = models.IntegerField(default=0, help_text="Grilled salmon fillet with a tri-colored peppercorn citrus sauce.")
+    email = models.EmailField()
     comments = models.TextField("Any Questions or Comments", blank=True, null=True)
+
+class DinnerChoice(TimeStampedModel):
+    DINNER_CHOICES = (
+        ("turkey", "Stuffed Turkey Roulade"),
+        ("salmon", "Citrus Grilled Salmon"),
+    )
+    rsvp = models.ForeignKey(Rsvp)
+    name = models.CharField(max_length=64)
+    dinner_choice = models.CharField(max_length=16, choices=DINNER_CHOICES, default="turkey")
